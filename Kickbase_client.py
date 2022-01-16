55bfba34d1f8fd96aefaa36fcfb603d8580ac60f
Josepp <94930756+SeppWolfrich@users.noreply.github.com>
2022-01-16 13:37:41 +0100

    Update Kickbase_client.py

--- Kickbase_client.py
+++ Kickbase_client.py
@@ -1,22 +1,22 @@
-# Standard Libraries-----------------------------------------------
+## Standard Libraries-----------------------------------------------
 import pandas as pd
 import numpy as np
 import streamlit as st
 import altair as alt
 import matplotlib.pyplot as plt
 
-# Bundesliga Libraries---------------------------------------------
+## Bundesliga Libraries---------------------------------------------
 from soccer_data_api import SoccerDataAPI # Bundesliga standings
 import requests # Scrape Bundesliga fixtures
 import lxml.html as lh # Scrape Bundesliga fixtures
 
 
-# Bundesliga Table ------------------------------------------------
+## SCRAPE DATA
 # Get bundesliga standing from API
 bundesliga_standing = pd.DataFrame(SoccerDataAPI().bundesliga()).iloc[: , :-1]  #current bundesliga table
 
 
-# Player data ------------------------------------------------
+#Scaping Ligainsider page
 ligainsider = 'https://www.ligainsider.de/stats/kickbase/marktwerte/gesamt/'
 page_player_information = requests.get(ligainsider) #Create a handle, page, to handle the contents of the website
 
@@ -88,11 +88,12 @@ Ligainsider.loc[Ligainsider.Verein == '1. FC Union Berlin', 'Verein'] = 'Union B
 Ligainsider.loc[Ligainsider.Verein == 'RB Leipzig', 'Verein'] = 'RB Leipzig'#
 Ligainsider.loc[Ligainsider.Verein == 'Borussia Mönchengladbach', 'Verein'] = "M'Gladbach" #
 
+#Ligainsider_final = Ligainsider
 Ligainsider_final = pd.merge(Ligainsider, bundesliga_standing, left_on='Verein',right_on='team') 
 Ligainsider_final = Ligainsider_final.drop('Verein',1)
 
 
-# Cleaning and converting ------------------------------------------------
+# Cleaning and converting
 Ligainsider_final['Spieler'] = [x.replace('\n', '') for x in Ligainsider_final['Spieler']] #removes /n 
 Ligainsider_final['Spieler'] = Ligainsider_final['Spieler'].astype(str)
 
@@ -106,7 +107,7 @@ Ligainsider_final['Punkteschnitt'] = Ligainsider_final['Gesamtpunkte']/Ligainsid
 Ligainsider_final['Punkteschnitt'] = Ligainsider_final['Punkteschnitt'].fillna(0)
 Ligainsider_final['Punkteschnitt'] = Ligainsider_final['Punkteschnitt'].round(2)
 
-Ligainsider_final['Marktwert'] = [x.replace('.', '') for x in Ligainsider_final['Marktwert']] #remove dots
+Ligainsider_final['Marktwert'] = [x.replace('.', '') for x in Ligainsider_final['Marktwert']] #remove commas or dots
 Ligainsider_final['Marktwert'] = Ligainsider_final['Marktwert'].str.replace('€', '') #remove € sign
 Ligainsider_final['Marktwert'] = Ligainsider_final['Marktwert'].astype(int)
 
@@ -117,9 +118,9 @@ Ligainsider_final.loc[Ligainsider_final.PreisProPunkt == np.inf, 'PreisProPunkt'
 df = Ligainsider_final
 
 
-# Visualise ------------------------------------------------
+# Visualise
 st.set_page_config(layout="wide") # page expands to full width
-st.title("Kickbase Analyser v1.1 (WIP)")
+st.title("Kickbase Analyser v1.2 (WIP)")
 
 # General analysis
 #st.header('Generelle Analyse aller Spieler der Bundesliga unabhängig vom Verein')
@@ -165,7 +166,6 @@ col2.metric("Verteidiger Punkte", df1.loc[(df1['Position'] == 'Abwehrspieler') &
 col3.metric("Mittelfeld Punkte", df1.loc[(df1['Position'] == 'Mittelfeldspieler') & (df1['Einsätze'] != 0), 'Gesamtpunkte'].mean().round(2))
 col4.metric("Stürmer Punkte", df1.loc[(df1['Position'] == 'Stürmer') & (df1['Einsätze'] != 0), 'Gesamtpunkte'].mean().round(2))
 
-st.caption('Durchschnittliche Gesamtpunkte aller Spieler des ausgewählten Teams mit mehr als 0 Einsätzen.')
 
 # Per Team Visuals
 st.altair_chart(alt.Chart(df1).mark_bar().encode(
@@ -189,52 +189,58 @@ st.altair_chart(alt.Chart(df1).mark_bar().encode(
 
 # Per Player Visuals
 st.subheader('Spieler Analyse')
-st.caption('Ausgewählter Spieler wird mit allen anderen Spielern der gleichen Position verglichen. Ausgewählter Spieler ist rot markiert. Spieler unterhalb der orangenen Linie erzielen überproportional viele Punkte gemessen an ihrem Marktwert.')
+st.caption('Ausgewählter Spieler wird mit allen anderen Spielern der gleichen Position verglichen. Ausgewählter Spieler ist rot markeiert. Spieler unterhalb der orangenen Linie erzielen durschnittlich überproportional viele Punkte für ihren Marktwert.')
 
-selected_player = st.selectbox("Select Player", (df['Spieler'].unique()))
+#selected_player = st.multiselect("Select Player", (df['Spieler'].unique())) #single selection
+#df2 = df[df['Spieler']==[selected_player]] #select player based on selection in col1
 
-df2 = df[df['Spieler']==selected_player] #select player based on selection in col1
+selected_player = st.multiselect("Select Player", 
+                                options = df['Spieler'].unique(), #df['Spieler'].unique()
+                                default = df['Spieler'][0]) #default = ['Robert Lewandowksi','Erling Haaland']
+st.caption('Für einen genauen Vergleich, bitte nur Spieler der gleichen Position auswählen. Wenn kein Spieler ausgewählt ist, resultiert das in einem Error.')
 
+
+df2 = df[df['Spieler'].isin(selected_player)] #filtering dataframe by list selected in selected_players
 df_base = df.loc[df['Position'] == df2.iloc[0][1]] #filter dataset based on position
 
 
-chart = alt.Chart(df_base).mark_circle(size=60).encode(
+# Plotting Gesamtpunkte v Marktwert -----------
+chart = alt.Chart(df_base).mark_circle(size=60).encode( #chart for all players
     x='Gesamtpunkte',
     y='Marktwert',
-    #color='orange',
-    #size = 'Punkteschnitt',
-    tooltip=['Spieler', 'Gesamtpunkte', 'Marktwert', 'Position','Punkteschnitt', 'team']
+    color=alt.value('grey'), #defaults all players to grey
+    #size = 'Einsätze',
+    tooltip=['Spieler', 'Gesamtpunkte','Punkteschnitt', 'Marktwert', 'Einsätze' ,'Position','team']
 )#.interactive()
 
-chart2 = alt.Chart(df2).mark_circle(size=60).encode(
+chart2 = alt.Chart(df2).mark_circle(size=60).encode( #chart for selected players
     x='Gesamtpunkte',
     y='Marktwert',
-    color=alt.value('red'),
-    #color='Position',
+    #color=alt.value('red'),
+    color='Spieler', #each player is assingned one color
     #size = 'Punkteschnitt',
-    tooltip=['Spieler', 'Gesamtpunkte', 'Marktwert', 'Position','Punkteschnitt', 'team']
+    tooltip=['Spieler', 'Gesamtpunkte','Punkteschnitt', 'Marktwert', 'Einsätze' ,'Position','team']
 )#.interactive()
 
 st.altair_chart((chart + chart2 + chart.transform_regression('Gesamtpunkte', 'Marktwert').mark_line(color="orange")), use_container_width = True)
 
 
 # Plotting Punkteschnitt v Marktwert -----------
-chart3 = alt.Chart(df_base).mark_circle(size=60).encode(
+chart3 = alt.Chart(df_base).mark_circle(size=60).encode( #chart for all players
     x='Punkteschnitt',
     y='Marktwert',
-    #color='orange',
+    color=alt.value('grey'), #defaults all players to grey
     #size = 'Punkteschnitt',
-    tooltip=['Spieler','Gesamtpunkte', 'Marktwert', 'Position','Punkteschnitt', 'team']
+    tooltip=['Spieler', 'Gesamtpunkte','Punkteschnitt', 'Marktwert', 'Einsätze' ,'Position','team']
 )#.interactive()
 
-chart4 = alt.Chart(df2).mark_circle(size=60).encode(
+chart4 = alt.Chart(df2).mark_circle(size=60).encode( #chart for selected players
     x='Punkteschnitt',
     y='Marktwert',
-    color=alt.value('red'),
-    #color='Position',
+    #color=alt.value('red'),
+    color='Spieler', #each player is assingned one color
     #size = 'Punkteschnitt',
-    tooltip=['Spieler','Punkteschnitt', 'Marktwert', 'Position','Gesamtpunkte', 'team']
+    tooltip=['Spieler', 'Gesamtpunkte','Punkteschnitt', 'Marktwert', 'Einsätze' ,'Position','team']
 )#.interactive()
 
 st.altair_chart((chart3 + chart4 + chart3.transform_regression('Punkteschnitt', 'Marktwert').mark_line(color="orange")), use_container_width = True)
-#+ chart.transform_regression('Gesamtpunkte', 'Marktwert', method="poly").mark_line()
