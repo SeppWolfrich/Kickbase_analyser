090b92f98399dc2efcf3840db7693e703fb72132
Josepp <94930756+SeppWolfrich@users.noreply.github.com>
2022-07-10 21:47:24 +0200

    Update Kickbase_client.py

--- Kickbase_client.py
+++ Kickbase_client.py
@@ -121,7 +121,8 @@ df = Ligainsider_final
 # VISUALISATION
 st.set_page_config(layout="wide") # page expands to full width
 st.title("Kickbase Analyser v1.2")
-st.caption('By [Sepp Wolfrich](https://twitter.com/SeppWolfrich). Feedback gerne direkt an mich via [Twitter](https://twitter.com/SeppWolfrich) oder [Mail](joseppwolfrich@gmail.com) !')
+st.caption('By [Sepp Wolfrich](https://twitter.com/SeppWolfrich). Feedback gerne direkt an mich via [Twitter](https://twitter.com/SeppWolfrich)')
+st.markdown('<a href="mailto:joseppwolfrich@gmail.com?subject=Kickbase Analyser Feedback!">Mail!</a>', unsafe_allow_html=True)
 
 # General analysis
 #st.header('Generelle Analyse aller Spieler der Bundesliga unabhängig vom Verein')
